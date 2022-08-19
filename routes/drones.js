416a88f476be22282ae73be95b602edb10d93b86
Paul Nizet <polonizet@gmail.com>
2022-08-19 16:48:27 +0200

    finish Remove drone -- All CRUD is done

--- routes/drones.js
+++ routes/drones.js
@@ -39,22 +39,31 @@ router.post('/drones', async (req, res, next) => {
 
 router.patch('/drones/:id', async (req, res, next) => {
   // Iteration #4: Update the drone
-  const { name, propellers, maxSpeed } = req.body
-  const drone = await Drone.findByIdAndUpdate(
-    req.params.id,
-    {
-      name,
-      propellers,
-      maxSpeed
-    },
-    { new: true }
-  )
-  res.json({ drone })
+  try {
+
+    const { name, propellers, maxSpeed } = req.body
+    const drone = await Drone.findByIdAndUpdate(
+      req.params.id,
+      {
+        name,
+        propellers,
+        maxSpeed
+      },
+      { new: true }
+    )
+    res.result(201).json({ drone })
+  } catch (error) {
+    if (!req.params.id) {
+      res.status(404).json({ error: 'No id provided' })
+    }
+    next(error)
+    res.status(400).json({ error })
+  }
 })
 
-router.delete('/drones/:id', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+router.delete('/drones/:id', async (req, res, next) => {
+  await Drone.findByIdAndDelete(req.params.id)
+  res.sendStatus(204)
 })
 
 module.exports = router
